f52dcccb458012ac49eb8cc21a0f2b28b367bc00
AlexanderTirik <sasha.tirik@gmail.com>
2022-02-08 22:06:22 +0200

    create tezos slice

---
+++ app/tezosSlice.ts
@@ -0,0 +1,29 @@
+import {
+    createSlice
+  } from '@reduxjs/toolkit';
+import { TezosToolkit } from '@taquito/taquito';
+import { RPCURL } from '../config/constants';
+  
+  export type TezosState = {
+    tezos: TezosToolkit;
+  };
+  
+  const initialState: TezosState = {
+    tezos: new TezosToolkit(RPCURL)
+  };
+  
+  export const tezosSlice = createSlice({
+    name: 'tezos',
+    initialState,
+    reducers: {
+      disconnect: (state) => {
+        state.tezos = new TezosToolkit(RPCURL);
+      }
+    },
+  });
+
+  export const {
+    disconnect
+  } = tezosSlice.actions;
+    
+  export default tezosSlice.reducer;
