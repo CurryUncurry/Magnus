58476e598ec3e1cbd0ae3266c14b478a040d61cb
AlexanderTirik <sasha.tirik@gmail.com>
2022-02-08 17:28:38 +0200

    add chackra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "../styles/global.sass";
 import { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../app/store";
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <ChakraProvider>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ChakraProvider>
   );
 }
